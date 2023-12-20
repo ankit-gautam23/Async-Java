d2f156b357cc109e7ed068bfeb25037eabfa15eb
ankit-gautam23 <ankit.gautamp23@gmail.com>
2023-12-20 18:42:00 +0530

    Some complex scenerios

---
+++ For Real/ComplexSceneriosDemo.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ComplexSceneriosDemo {
+
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(4);
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException{
+        System.out.println(executeAsync().get());
+    }
+
+    public static CompletableFuture<String> executeAsync(){
+        CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(()->"result1");
+        CompletableFuture<String> cf2;
+
+        CompletableFuture<String> cf3 = cf1.thenComposeAsync(
+            (result)->CompletableFuture.supplyAsync(() -> result + "result2"));
+
+
+        CompletableFuture<CompletableFuture<String>> cf4 = cf1.thenApplyAsync((result)->CompletableFuture.supplyAsync(()-> result + "result n"));
+        return cf3;
+
+    }
+
+}
